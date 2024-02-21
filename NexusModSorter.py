b2df09e53c24f4c1ea33631f786e9d5e617b0887
theRealBassist <rangercam98@gmail.com>
2024-02-21 14:28:58 -0500

    modified:   NexusModSorter.py     Updated to use Selenium and Beautiful Soup to dynamically gather the mod categories available on the Nexus. It currently uses a headless Firefox client, but if necessary I might add the ability for it to use chrome as well.

--- NexusModSorter.py
+++ NexusModSorter.py
@@ -1,20 +1,43 @@
-from importlib.resources import path
 from os import mkdir
 import os.path
-from pathlib import Path
 import requests
 from bs4 import BeautifulSoup
 import shutil
 from difflib import SequenceMatcher
-from sys import platform
 import tkinter
 import threading
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.firefox.options import Options
 
 
 modsToSearch = []  
 threads = []
+validCategories = []
+    
+def getValidCategories():
+    firefox_options = Options()
+    firefox_options.add_argument("--headless")
+    firefox_options.page_load_strategy = 'normal'
+    firefox_options.add_argument("--headless")
+    firefox_options.page_load_strategy = 'normal'
 
-validCategories = ["skyrimspecialedition", "skyrim", "fallout4", "newvegas", "oblivion", "stardewvalley", "fallout3", "cyberpunk2077", "witcher3", "dragonage", "mountandblade2bannerlord", "bladeandsorcery", "monsterhunterworld", "morrowind", "dragonage2"]
+    driver = webdriver.Firefox(options=firefox_options)
+    driver.get ("https://www.nexusmods.com/games")
+    driver.implicitly_wait(5)
+    expand = driver.find_element(By.XPATH, "/html/body/div[1]/section/div/div[2]/div[2]/div[1]/a")
+    driver.execute_script("arguments[0].click();", expand)
+    driver.implicitly_wait(5)
+    soup = BeautifulSoup(driver.page_source, 'lxml')
+    driver.quit()
+
+    gameNames = soup.find_all(class_="mod-image", href=True)
+
+    for tag in gameNames:
+        tagString = str(tag)
+        gameUrl = tagString.split("<a class=\"mod-image\" href=\"/", 1)[1].split("\"", 1)[0].strip()
+        validCategories.append(gameUrl)
+    print ("Gathered Valid Directories")
 
 def getDirectory(): #responsible for displaying the UI and collecting desired selections
     
@@ -28,6 +51,7 @@ def getDirectory(): #responsible for displaying the UI and collecting desired se
         except:
             workingDir = input("Enter the full file path of the folder where your mods are stored:\n")
         if os.path.exists(workingDir): #pathlib is incompatible with pyinstaller, so I'm using os.path
+            print ("Gathered Working Directory")
             return workingDir
         else:
             print("This is not a valid directory! Please try again.")
@@ -44,12 +68,14 @@ def getModID(filename):
         getModID(unmoddedModID)
     elif unmoddedModID[0].isdigit():
         modID = unmoddedModID.split(sep,1)[0].strip()
+        print (f'ModID = {modID}')
         return(modID)
     else:
         try:
             unmoddedModID = unmoddedModID.split(sep,1)[1].strip()
             if unmoddedModID[0].isdigit():
                 modID = unmoddedModID.split(sep,1)[0].strip()
+                print (f'ModID = {modID}')
                 return(modID)
             else:
                 print("This ModID is broken: " + unmoddedModID)
@@ -68,6 +94,7 @@ def isSimilar(modFileName, modName):
         isContained = False
 
     if similarityRatio >= 0.70 or isContained == True:
+        print (f'{modFileName} and {modName} are Similar!')
         return True
     else:
         return False
@@ -100,6 +127,7 @@ def titleSearch (modID, modFileName, categoryToSearch):
     modName = soup.find('title').get_text().split(sep,1)[0].split(" at ")[0]
     similarity = isSimilar(modFileName, modName)
     if similarity == True:
+        print (f'Found title for {modName} in {categoryToSearch}')
         return [categoryToSearch, modName]
     else:
         return False
@@ -108,11 +136,10 @@ def iterateCategories (mod, category=0):
     modID = mod[0]
     filename = mod[1]
     currentCategory = category
-    print(f'ModID = {modID}, filename = {filename}, currentCategory = {currentCategory}')
     while currentCategory < len(validCategories):
         foundTitle = titleSearch(modID, filename, validCategories[currentCategory])
         if  foundTitle == False:
-            print (f'Not {validCategories[currentCategory]}')
+            print (f'Not found in {validCategories[currentCategory]}')
             currentCategory += 1
             iterateCategories(mod, currentCategory)
         else:
@@ -126,13 +153,12 @@ def iterateCategories (mod, category=0):
 def parallelModSearch():
     global foundModTitles
     foundModTitles = []
-    for mod in modsToSearch:
-        print(f'mod = {mod}')
+    for mod in modsToSearch: #creates the individual threads to search for the correct mod category
         thread = threading.Thread(target=iterateCategories, args=(mod, 0))
         threads.append(thread)
-    for thread in threads:
+    for thread in threads: #starts each created thread
         thread.start()
-    for thread in threading.enumerate():
+    for thread in threading.enumerate(): #waits for and eventually shuts down each thread
         if thread.daemon:
             continue
         try:
@@ -143,12 +169,9 @@ def parallelModSearch():
                 continue
             else:
                 raise
-    #return foundModTitles
-
-
-
 
 workingDir = getDirectory()
+getValidCategories()
 unsortedPath = os.path.join(workingDir, "UNSORTED")
 
 try:
@@ -162,8 +185,6 @@ for file in os.listdir(workingDir):
     filePath = os.path.join(workingDir, file)
     if os.path.isdir(filePath) == False:
         print("filename = " + filename)
-
-        print("I am working with the file: " + filename)
 
         modID = getModID(filename)
         if modID == 0:
@@ -180,7 +201,6 @@ for file in os.listdir(workingDir):
         modsToSearch.append(output)
 
 parallelModSearch()
-print (foundModTitles)
 for mod in foundModTitles:
     categoryPath = os.path.join(workingDir, mod[3].upper())
     if not os.path.isdir(categoryPath):
